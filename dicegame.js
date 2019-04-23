163b205648efd3924ed97e66c8f85a6627160164
Mitchell Mittelstedt <mmittelstedt@icloud.com>
2019-04-23 15:34:32 -0500

    added chance of enemy and loot appearing and the dice that determine those chances

--- dicegame.js
+++ dicegame.js
@@ -48,23 +48,29 @@ function getVitality(vitality, goldAmount) {
 function updateVitalityCounter(vitality) {
     vitality = vitality - enemyDamage();
     vitality = vitality + attackDamage();
-    return vitality
+    return vitality;
 }
 
 
 function updateGoldCounter(playerGold) {
-    
-    let goldFound = rollDie(12);
-    playerGold += goldFound;
-    return playerGold;
+    if(ifLoot = true) {
+        let goldFound = rollDie(12);
+        playerGold += goldFound;
+        return playerGold;        
+    }
+    else {
+        
+    }
 }
 
 function enemyDamage() {
     if(ifEnemy = true) {
-      let damage = rollDie(15);
+      let damage = rollDie(25);
       return damage;  
     }
-    
+    else {
+        
+    }   
 }
 
 function ifEnemy() {
@@ -93,58 +99,3 @@ function attackDamage() {
 }
 
 playGame();
-
-
-
-
-
-
-
-// function increaseTurnCounter(thanos) {
-//     thanos++;
-//     return thanos;
-// }
-
-
-
-
-
-// window.addEventListener( 'DOMContentLoaded', function () {
-	
-//     const buttonRoolDice = document.querySelector( '.dice-roll' );
-
-// function rollDice () {
-
-//     const diceSide1 = document.getElementById( 'dice-side-1' );
-//     const diceSide2 = document.getElementById( 'dice-side-2' );
-//     const diceSide3 = document.getElementById( 'dice-side-3');
-//     const status = document.getElementById( 'status' );
-
-//     const side1 = rollDie(20);
-//     alert("You just rolled a " + side1);
-//     // return;
-//     const diceTotal = side1 + side2 + side3;
-
-//     diceSide1.innerHTML = side1;
-//     diceSide2.innerHTML = side2;
-//     diceSide3.innerHTML = side3;
-
-//     status.innerHTML = 'You rolled ' + diceTotal + '.';
-
-//     if ( side1 === side2 ) {
-//         status.innerHTML += ' Doubles! You get a free turn!';
-//     }
-
-//     else if (side1 === side3) {
-
-//     }
-// }
-
-// buttonRoolDice.addEventListener( 'click', rollDice, false );
-
-// }, false);    
-
-
-
-// const side2 = Math.floor( Math.random() * 6 ) + 1;
-// const side3 = Math.floor( Math.random() * 12 ) + 1;
