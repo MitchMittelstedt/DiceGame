8b858c2ff3c2550e842bfdcbfff052a2b1e0c563
Mitchell Mittelstedt <mmittelstedt@icloud.com>
2019-04-23 14:24:38 -0500

    added enemy damage and attack healing + dice rolls towards those

--- dicegame.js
+++ dicegame.js
@@ -2,7 +2,8 @@ function playGame() { // master function
     // set up phase
     let goldAmount = rollDie(200);
     let movesLeft = 200;
-    let vitality = getVitality(goldAmount);
+    let vitality = 0;
+    vitality = getVitality(vitality,goldAmount);
 
     // game play (maybe loop?)
     while(movesLeft > 0) {    // do ONE turn
@@ -16,58 +17,86 @@ function playGame() { // master function
         console.log("You now have " + goldAmount + " gold.");
 
         vitality = updateVitalityCounter(vitality);
+        
         console.log("You now have " + vitality + " vitality.")
 
-    }""
+    }
 
     // end game stuff
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 function rollDie(numberOfSides) {
     let result = Math.floor( Math.random() * numberOfSides) + 1;
     return result;
 }
 
-function getVitality(goldAmount) {
+function getVitality(vitality, goldAmount) {
     if(goldAmount > 150) {
-        vitality = 70;
+        vitality = 25;
+        return vitality;
     }
     else if(goldAmount > 100 && goldAmount <= 150) {
-        vitality = 85;
+        vitality = 50;
+        return vitality;
     }
     else {
         vitality = 100;
+        return vitality;
     }
 }
 
+function updateVitalityCounter(vitality) {
+    vitality = vitality - enemyDamage();
+    vitality = vitality + attackDamage();
+    return vitality
+}
+
+
 function updateGoldCounter(playerGold) {
+    
     let goldFound = rollDie(12);
     playerGold += goldFound;
     return playerGold;
 }
 
 function enemyDamage() {
-    let damage = rollDie(15);
-    return damage
-
+    if(ifEnemy = true) {
+      let damage = rollDie(15);
+      return damage;  
+    }
+    
 }
 
-function updateVitalityCounter(vitality) {
-    vitality -= enemyDamage()
-    return vitality
+function ifEnemy() {
+    let enemy = rollDice(10);
+    if(enemy > 5) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
+
+function ifLoot() {
+    let loot = rollDice(10);
+    if(loot > 5) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function attackDamage() {
+    let attack = rollDie(20);
+    return attack;
+}
+
+playGame();
+
+
+
+
 
 
 
@@ -76,7 +105,7 @@ function updateVitalityCounter(vitality) {
 //     return thanos;
 // }
 
-playGame();
+
 
 
 
